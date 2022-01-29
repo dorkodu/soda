6126ac59e9f180d3a25d000868b358201e771ead
Berk Cambaz <berkcambaz12321@gmail.com>
2022-01-29 17:52:12 +0300

    DOM diffing algorithm LGTM

--- src/index.ts
+++ src/index.ts
@@ -33,10 +33,14 @@ class Lucid {
       const component = {
         attrs: element.attrs,
         update: () => {
-          if (typeof element.tag === "function")
+          if (typeof element.tag === "function") {
+            const newDOM = component.__dom.cloneNode(true);
+            component.__dom.parentNode?.replaceChild(newDOM, component.__dom);
+            component.__dom = newDOM as HTMLElement;
             this._update(component.__dom as unknown as HTMLElement, element.tag(component))
+          }
         },
-        __dom: undefined
+        __dom: undefined as unknown as HTMLElement
       };
       this._render(dom, element.tag(component), component);
     }
@@ -48,7 +52,7 @@ class Lucid {
 
     for (const key in element.attrs) {
       if (key.startsWith("on")) {
-        elem.addEventListener(key.substring(2).toLocaleLowerCase(), (ev) => element.attrs[key](ev))
+        elem.addEventListener(key.substring(2).toLowerCase(), (ev) => element.attrs[key](ev))
       }
       else {
         elem.setAttribute(translate(key), element.attrs[key]);
@@ -62,7 +66,6 @@ class Lucid {
         this._render(elem, element.children[i], undefined);
       else
         elem.appendChild(document.createTextNode(element.children[i]));
-
     }
 
     dom.appendChild(elem);
@@ -74,30 +77,41 @@ class Lucid {
       console.log("TODO: Diff");
     }
 
-    // TODO: Property diff
+    for (const key in element.attrs) {
+      if (key.startsWith("on")) {
+        dom.addEventListener(key.substring(2).toLowerCase(), (ev) => element.attrs[key](ev))
+      }
+      else {
+        dom.setAttribute(translate(key), element.attrs[key]);
+      }
+    }
 
     for (let i = 0; i < element.children.length || i < dom.childNodes.length; ++i) {
       // Remove the excess amount of children
       if (!element.children[i]) { dom.removeChild(dom.childNodes[i--]); continue; }
 
+      console.log(dom.childNodes[i]);
+      console.log(element.children[i]);
+
       if (typeof element.children[i] === "object") {
+        if (!dom.childNodes[i]) {
+          dom.appendChild(document.createElement(element.children[i].tag));
+        }
+        else if (dom.childNodes[i].nodeType !== document.ELEMENT_NODE) {
+          dom.insertBefore(document.createElement(element.children[i].tag), dom.childNodes[i]);
+        }
+
         this._update(dom.childNodes[i] as HTMLElement, element.children[i]);
       }
       else if (typeof element.children[i] === "function") {
         console.log("TODO: Handle element children re-render")
       }
       else {
-        // DOM type is an element but the element child is a text node
-        if (dom.childNodes[i].nodeType === document.ELEMENT_NODE) {
-          dom.removeChild(dom.childNodes[i]);
-
-          if (dom.childNodes.length > 1 && dom.childNodes[i])
-            dom.insertBefore(document.createTextNode(element.children[i]), dom.childNodes[i])
-          else
-            dom.appendChild(document.createTextNode(element.children[i]));
-
-          i--;
+        if (!dom.childNodes[i]) {
+          dom.appendChild(document.createTextNode(element.children[i]));
         }
+        else if (dom.childNodes[i].nodeType !== document.TEXT_NODE)
+          dom.insertBefore(document.createTextNode(element.children[i]), dom.childNodes[i]);
         else if (dom.childNodes[i].nodeValue !== element.children[i]) {
           dom.childNodes[i].nodeValue = element.children[i];
         }

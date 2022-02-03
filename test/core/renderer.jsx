68f03c979e26f9cc874f701ec502c2fc32ce25a3
Berk Cambaz <berkcambaz12321@gmail.com>
2022-02-04 00:17:59 +0300

    Add more tests

--- test/core/renderer.jsx
+++ test/core/renderer.jsx
@@ -7,6 +7,8 @@ seekr.describe("Renderer", () => {
       document.body.removeChild(document.body.firstChild)
   })
 
+
+
   seekr.it("render basic", () => {
     function App(component) {
       return <div>Hello, world!</div>
@@ -16,6 +18,8 @@ seekr.describe("Renderer", () => {
 
     return document.body.innerHTML === "<div>Hello, world!</div>"
   })
+
+
 
   seekr.it("render with variables", () => {
     function App(component) {
@@ -27,6 +31,8 @@ seekr.describe("Renderer", () => {
 
     return document.body.innerHTML === "<div>Count: 0</div>"
   })
+
+
 
   seekr.it("render by passing attributes", () => {
     function App(component) {
@@ -43,6 +49,8 @@ seekr.describe("Renderer", () => {
     return document.body.innerHTML === "<div><div>Hi!</div></div>"
   })
 
+
+
   seekr.it("store state and update", () => {
     function App(component) {
       component.state = component.state || { count: 0 }
@@ -57,6 +65,8 @@ seekr.describe("Renderer", () => {
 
     return document.body.innerHTML === "<div>Count: 1</div>"
   })
+
+
 
   seekr.it("render children", () => {
     function App(component) {
@@ -76,6 +86,8 @@ seekr.describe("Renderer", () => {
     return document.body.innerHTML === "<div>Hello, world!<div>Hello, world!<div>Hello, world!</div></div></div>"
   })
 
+
+
   seekr.it("render component as children", () => {
     function App(component) {
       return <div><Greeting /></div>
@@ -89,6 +101,8 @@ seekr.describe("Renderer", () => {
 
     return document.body.innerHTML === "<div><div>Hello, world!</div></div>"
   })
+
+
 
   seekr.it("render an array as children", () => {
     const letters = ["s", "o", "d", "a"]
@@ -105,6 +119,8 @@ seekr.describe("Renderer", () => {
 
     return document.body.innerHTML === "<div><div>s</div><div>o</div><div>d</div><div>a</div></div>"
   })
+
+
 
   seekr.it("render an array of components as children", () => {
     const letters = ["s", "o", "d", "a"]
@@ -127,6 +143,8 @@ seekr.describe("Renderer", () => {
     return document.body.innerHTML === "<div><div>s</div><div>o</div><div>d</div><div>a</div></div>"
   })
 
+
+
   seekr.it("render with a condition", () => {
     let count = 1;
 
@@ -145,7 +163,9 @@ seekr.describe("Renderer", () => {
     return document.body.innerHTML === "<div>Count is 2</div>"
   })
 
-  seekr.it("render children then add 1", () => {
+
+
+  seekr.it("render children then add last", () => {
     const letters = ["s", "o", "d", "a"]
 
     function App(component) {
@@ -170,7 +190,9 @@ seekr.describe("Renderer", () => {
     return document.body.innerHTML === "<div><button>Add!</button><div>s</div><div>o</div><div>d</div><div>a</div><div>!</div></div>"
   })
 
-  seekr.it("render children as components then add 1", () => {
+
+
+  seekr.it("render children as components then add last", () => {
     const letters = ["s", "o", "d", "a"]
 
     function App(component) {
@@ -200,7 +222,9 @@ seekr.describe("Renderer", () => {
     return document.body.innerHTML === "<div><button>Add!</button><div>s</div><div>o</div><div>d</div><div>a</div><div>!</div></div>"
   })
 
-  seekr.it("render children then remove 1", () => {
+
+
+  seekr.it("render children then remove last", () => {
     const letters = ["s", "o", "d", "a", "!"]
 
     function App(component) {
@@ -221,13 +245,15 @@ seekr.describe("Renderer", () => {
 
     soda.render(<App />, document.body)
 
-    // Should add a letter
+    // Should remove a letter
     document.body.firstChild.firstChild.click();
 
     return document.body.innerHTML === "<div><button>Remove!</button><div><div>s</div><div>o</div><div>d</div><div>a</div></div></div>"
   })
 
-  seekr.it("render children as components then remove 1", () => {
+
+
+  seekr.it("render children as components then remove last", () => {
     const letters = ["s", "o", "d", "a", "!"]
 
     function App(component) {
@@ -253,7 +279,153 @@ seekr.describe("Renderer", () => {
 
     soda.render(<App />, document.body)
 
+    // Should remove a letter
+    document.body.firstChild.firstChild.click();
+
+    return document.body.innerHTML === "<div><button>Remove!</button><div><div>s</div><div>o</div><div>d</div><div>a</div></div></div>"
+  })
+
+
+
+  seekr.it("render children then add first", () => {
+    const letters = [
+      { id: 1, letter: "o" },
+      { id: 2, letter: "d" },
+      { id: 3, letter: "a" }
+    ]
+
+    function App(component) {
+      const addLetter = () => {
+        letters.splice(0, 0, { id: 0, letter: "s" });
+        component.update();
+      }
+
+      return (
+        <div>
+          <button onclick={addLetter}>Add!</button>
+          <div>
+            {letters.map((letter) => <div key={letter.id}>{letter.letter}</div>)}
+          </div>
+        </div>
+      )
+    }
+
+    soda.render(<App />, document.body)
+
     // Should add a letter
+    document.body.firstChild.firstChild.click();
+
+    return document.body.innerHTML === "<div><button>Add!</button><div><div>s</div><div>o</div><div>d</div><div>a</div></div></div>"
+  })
+
+
+
+  seekr.it("render children as components then add first", () => {
+    const letters = [
+      { id: 1, letter: "o" },
+      { id: 2, letter: "d" },
+      { id: 3, letter: "a" }
+    ]
+
+    function App(component) {
+      const addLetter = () => {
+        letters.splice(0, 0, { id: 0, letter: "s" });
+        component.update();
+      }
+
+      return (
+        <div>
+          <button onclick={addLetter}>Add!</button>
+          <div>
+            {letters.map((letter) => <Letter key={letter.id} letter={letter.letter} />)}
+          </div>
+        </div>
+      )
+    }
+
+    function Letter(component) {
+      const letter = component.attrs.letter;
+      return <div>{letter}</div>
+    }
+
+    soda.render(<App />, document.body)
+
+    // Should add a letter
+    document.body.firstChild.firstChild.click();
+
+    return document.body.innerHTML === "<div><button>Add!</button><div><div>s</div><div>o</div><div>d</div><div>a</div></div></div>"
+  })
+
+
+
+  seekr.it("render children then remove first", () => {
+    const letters = [
+      { id: 0, letter: "!" },
+      { id: 1, letter: "s" },
+      { id: 2, letter: "o" },
+      { id: 3, letter: "d" },
+      { id: 4, letter: "a" }
+    ]
+
+    function App(component) {
+      const removeLetter = () => {
+        letters.splice(0, 1);
+        component.update();
+      }
+
+      return (
+        <div>
+          <button onclick={removeLetter}>Remove!</button>
+          <div>
+            {letters.map((letter) => <div key={letter.id}>{letter.letter}</div>)}
+          </div>
+        </div>
+      )
+    }
+
+    soda.render(<App />, document.body)
+
+    // Should remove a letter
+    document.body.firstChild.firstChild.click();
+
+    return document.body.innerHTML === "<div><button>Remove!</button><div><div>s</div><div>o</div><div>d</div><div>a</div></div></div>"
+  })
+
+
+
+  seekr.it("render children as components then remove first", () => {
+    const letters = [
+      { id: 0, letter: "!" },
+      { id: 1, letter: "s" },
+      { id: 2, letter: "o" },
+      { id: 3, letter: "d" },
+      { id: 4, letter: "a" }
+    ]
+
+    function App(component) {
+      const removeLetter = () => {
+        letters.splice(0, 1);
+        component.update();
+      }
+
+      return (
+        <div>
+          <button onclick={removeLetter}>Remove!</button>
+          <div>
+            {letters.map((letter) => <Letter key={letter.id} letter={letter.letter} />)}
+          </div>
+        </div>
+      )
+    }
+
+    function Letter(component) {
+      const letter = component.attrs.letter;
+      return <div>{letter}</div>
+    }
+
+    soda.render(<App />, document.body)
+
+    // Should remove a letter
     document.body.firstChild.firstChild.click();
 
     return document.body.innerHTML === "<div><button>Remove!</button><div><div>s</div><div>o</div><div>d</div><div>a</div></div></div>"

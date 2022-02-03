14be7e12fd7aede5e19b7e91f0ad86404c9cdc5d
Berk Cambaz <berkcambaz12321@gmail.com>
2022-02-03 19:07:10 +0300

    Add basic test

---
+++ test/core/renderer.jsx
@@ -0,0 +1,20 @@
+import { seekr } from "../seekr";
+import { soda } from "../../lib/index";
+
+seekr.describe("Renderer", () => {
+  seekr.beforeEach(() => {
+    while (document.body.firstChild)
+      document.body.removeChild(document.body.firstChild)
+  })
+
+  seekr.it("render basic", () => {
+    function App(component) {
+      return <div>Hello, world!</div>
+    }
+
+    soda.render(<App />, document.body)
+
+    //return document.body.innerHTML === "<div>Hello, world!</div>"
+    return new Date().getSeconds() % 2;
+  })
+})
---
+++ test/seekr.js
@@ -0,0 +1,62 @@
+class Seekr {
+  constructor() {
+    let it;
+    let beforeEach;
+
+    this.describe = function (name, cb) {
+      it = [];
+      beforeEach = undefined;
+
+      let tests = [];
+      let pass = 0;
+      let fail = 0;
+
+      cb();
+
+      let totalTime = 0;
+      let startTime = 0;
+      for (let i = 0; i < it.length; ++i) {
+        startTime = performance.now();
+        const result = it[i].cb();
+        totalTime += performance.now() - startTime;
+
+        if (result) {
+          ++pass;
+        }
+        else {
+          ++fail;
+        }
+
+        tests.push({ name: it[i].name, result })
+
+        beforeEach && beforeEach();
+      }
+
+      let string = "";
+      let style = [];
+
+      string += `${fail > 0 ? "%c FAIL " : "%c PASS "}%c ${name}`
+      style.push(fail > 0 ? "background-color:red;" : "background-color:green;", "")
+      for (let i = 0; i < tests.length; ++i) {
+        string += `\n%c${tests[i].result ? "✔️" : "❌"} ${tests[i].name}`
+        style.push("");
+      }
+      string += `\n\nTests: %c${pass} passed, %c${fail} failed`
+      style.push("color:green;", "color:red;")
+      string += `\n%cTime: ${totalTime / 1000}s`
+      style.push("");
+
+      console.log(string, ...style);
+    }
+
+    this.it = function (name, cb) {
+      it.push({ name, cb });
+    }
+
+    this.beforeEach = function (cb) {
+      beforeEach = cb;
+    }
+  }
+}
+
+export const seekr = new Seekr();

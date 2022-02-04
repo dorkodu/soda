2378cee58c3477dde3e3059dadbeedcd3fadd7a7
Berk Cambaz <berkcambaz12321@gmail.com>
2022-02-04 21:55:54 +0300

    Add ref tests

--- test/core/effect.jsx
+++ test/core/effect.jsx
@@ -1,2 +1,9 @@
 import { seekr } from "../seekr";
 import { soda } from "../../lib/index";
+
+seekr.describe("Effect", () => {
+  seekr.beforeEach(() => {
+    while (document.body.firstChild)
+      document.body.removeChild(document.body.firstChild)
+  })
+})

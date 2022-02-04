ab754265dd4522ba7085c6051bfd6773f2633cf7
Berk Cambaz <berkcambaz12321@gmail.com>
2022-02-04 20:33:21 +0300

    Add state tests

--- test/core/state.jsx
+++ test/core/state.jsx
@@ -35,4 +35,68 @@ seekr.describe("State", () => {
 
     return document.body.innerHTML === "<div>Count: 2</div>"
   })
+
+
+
+  seekr.it("basic set state without update using dontUpdate=true", () => {
+    function App(component) {
+      const [count, setCount] = soda.state(1);
+      return <div onClick={() => { setCount(count + 1, true) }}>Count: {count}</div>
+    }
+
+    soda.render(<App />, document.body)
+
+    // Should increase count by 1
+    document.body.firstChild.click();
+
+    return document.body.innerHTML === "<div>Count: 1</div>"
+  })
+
+
+
+  seekr.it("basic set state with update using dontUpdate=false", () => {
+    function App(component) {
+      const [count, setCount] = soda.state(1);
+      return <div onClick={() => { setCount(count + 1, false) }}>Count: {count}</div>
+    }
+
+    soda.render(<App />, document.body)
+
+    // Should increase count by 1
+    document.body.firstChild.click();
+
+    return document.body.innerHTML === "<div>Count: 2</div>"
+  })
+
+
+
+  seekr.it("basic set state with update using equality callback", () => {
+    function App(component) {
+      const [count, setCount] = soda.state(1, (prev, next) => prev === next);
+      return <div onClick={() => { setCount(count + 1) }}>Count: {count}</div>
+    }
+
+    soda.render(<App />, document.body)
+
+    // Should increase count by 1
+    document.body.firstChild.click();
+
+    return document.body.innerHTML === "<div>Count: 2</div>"
+  })
+
+
+
+  seekr.it("basic set state without update using equality callback", () => {
+    function App(component) {
+      const [count, setCount] = soda.state(1, (prev, next) => prev === next);
+      return <div onClick={() => { setCount(count) }}>Count: {count}</div>
+    }
+
+    soda.render(<App />, document.body)
+
+    // Should increase count by 1
+    document.body.firstChild.click();
+
+    return document.body.innerHTML === "<div>Count: 1</div>"
+  })
 })
